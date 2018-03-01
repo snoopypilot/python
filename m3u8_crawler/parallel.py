6b13fbdb3e7a4110f6718a5a00e3a3cf3cabdaa2
snoopypilot <reggie.tclai@gmail.com>
2018-03-01 15:41:35 +0900

    output summary, open video stream with Safari

--- m3u8_crawler/parallel.py
+++ m3u8_crawler/parallel.py
@@ -3,31 +3,39 @@
 from requests_futures.sessions import FuturesSession
 from concurrent.futures import wait
 import re
+import os
 
 session = FuturesSession(max_workers=100)
 
+# get html sources
 futures = [
-   session.get('http://widestream.io/embed-9' + str(num))
-   for num in range(100, 1000)
+   session.get('http://widestream.io/embed-' + str(num))
+   for num in range(9000, 10000)
 ]
 done, incomplete = wait(futures)
 
+# extract m3u8 sources
 m3u8 = []
 for r in futures:
    match = re.search(r'file:\"([^\"]+widestream[^\"]+m3u8[^\"]+)\"', r.result().text)
    if(match):
       m3u8.append(match.group(1))
 
-
+# check video source availability
 futures = [
    session.head(src)
    for src in m3u8
 ]
 done, incomplete = wait(futures)
 
-i = 0
+# output all working live streams
+it = iter(m3u8)
+count = 0
+
 for r in futures:
+   src = next(it)
    if(r.result().ok):
-      print(m3u8[i])
-   i += 1
-      
+      os.system('open -a Safari ' + src)
+      count += 1
+
+print(str(count) + ' live streams are extracted')
