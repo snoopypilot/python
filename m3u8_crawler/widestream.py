c679239ffa91eb8e3d309ef31e8920180c9457fe
snoopypilot <reggie.tclai@gmail.com>
2018-03-02 18:21:01 +0900

    callback

--- m3u8_crawler/widestream.py
+++ m3u8_crawler/widestream.py
@@ -2,40 +2,39 @@
 
 from requests_futures.sessions import FuturesSession
 from concurrent.futures import wait
+import requests
 import re
 import os
+import sys
+
+if(len(sys.argv)>1):
+   n = int(sys.argv[1])
+   if(len(sys.argv)>2):
+      p = True
+   else:
+      p = False
+else:
+   n = 9
+   p = False
+
 
 session = FuturesSession(max_workers=100)
 
+def main(sess, resp):
+   match = re.search(r'file:\"([^\"]+m3u8[^\"]+)\"', resp.text)
+   if(match):
+      src = match.group(1)
+      #print(src)
+      r = requests.get(src)
+      if(r.status_code == requests.codes.ok and r.headers['Content-Type'] < 'applications'):
+         if(p):
+            print(src)
+         else:
+            os.system('open -a Safari ' + src)
+
 # get html sources
 futures = [
-   session.get('http://widestream.io/embed-' + str(num))
-   for num in range(9000, 10000)
+   session.get('http://widestream.io/embed-' + str(num), background_callback=main)
+   for num in range(1000*n, 1000*(n+1))
 ]
 done, incomplete = wait(futures)
-
-# extract m3u8 sources
-m3u8 = []
-for r in futures:
-   match = re.search(r'file:\"([^\"]+widestream[^\"]+m3u8[^\"]+)\"', r.result().text)
-   if(match):
-      m3u8.append(match.group(1))
-
-# check video source availability
-futures = [
-   session.head(src)
-   for src in m3u8
-]
-done, incomplete = wait(futures)
-
-# output all working live streams
-it = iter(m3u8)
-count = 0
-
-for r in futures:
-   src = next(it)
-   if(r.result().ok):
-      os.system('open -a Safari ' + src)
-      count += 1
-
-print(str(count) + ' live streams are extracted')
